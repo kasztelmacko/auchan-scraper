0fbe78d605c87e4381924b91101393cc61439c5e
Sergio Cárcamo <scarcamo@users.noreply.github.com>
2024-03-10 17:06:29 +0100

    add linter and abs paths

--- pipelines.py
+++ pipelines.py
@@ -3,37 +3,43 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+import os
+import sqlite3
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 from scrapy.exceptions import DropItem
-import sqlite3
 
 
 class AuchanPipeline:
     def process_item(self, item, spider):
         return item
-    
+
+
 class DuplicatesPipeline:
     def __init__(self):
         self.names_seen = set()
-    
+
     def process_item(self, item, spider):
         adapter = ItemAdapter(item)
 
-        if adapter['name'] in self.names_seen:
-            raise DropItem(f'Duplicate item found: {item!r}')
+        if adapter["name"] in self.names_seen:
+            raise DropItem(f"Duplicate item found: {item!r}")
         else:
-            self.names_seen.add(adapter['name'])
+            self.names_seen.add(adapter["name"])
             return item
-        
+
+
 class SavingTosqlitePipeline(object):
     def __init__(self):
         self.create_connection()
 
     def create_connection(self):
         try:
-            self.connection = sqlite3.connect('../auchan.db')
+            current_path = os.path.dirname(os.path.realpath(__file__))
+            self.connection = sqlite3.connect(
+                os.path.join(current_path, "..", "auchan.db")
+            )
             self.curr = self.connection.cursor()
             print("Połączono z bazą danych")
         except sqlite3.Error as e:
@@ -44,13 +50,11 @@ class SavingTosqlitePipeline(object):
         print("Processing item in SavingTosqlitePipeline")
         self.store_db(item)
         return item
-    
+
     def store_db(self, item):
-        self.curr.execute(""" insert into auchan (product_name, category_name, price, volume) values (?, ?, ?, ?)""", (
-            item['name'],
-            item['category_name'],
-            item['price'],
-            item['volume']
-        ))
+        self.curr.execute(
+            """ insert into auchan (product_name, category_name, price, volume) values (?, ?, ?, ?)""",
+            (item["name"], item["category_name"], item["price"], item["volume"]),
+        )
         self.connection.commit()
         return self.curr
